e5477d85a572f3ed8cab07e22c842737d8eed9fc
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2015-04-19 13:23:13 -0400

    update

--- esvm_compute_models.m
+++ esvm_compute_models.m
@@ -1,16 +1,16 @@
 function esvm_compute_models(imgsdir, masksdir, negdir, outdir)
 addpath(genpath('~/projects/001_ESVM/ESVM/esvm_matching_vanilla/'));
 if ~exist('negdir', 'var')
-  negdir = '/IUS/homes4/rohytg/projects/001_ESVM/datasets/negativeImages_1K'
+  negdir = '/IUS/homes4/rohytg/work/data/002_ExtendedPAL/corpus_mods/esvm/trainNneg/'
 end
 if ~exist('masksdir', 'var')
-  masksdir = '/IUS/vmr105/rohytg/projects/003_SelfieSeg/009_BgMatch/dataset/PeopleAtLandmarks/masks'
+%  masksdir = '/IUS/vmr105/rohytg/projects/003_SelfieSeg/009_BgMatch/dataset/PeopleAtLandmarks/masks'
 end
 if ~exist('imgsdir', 'var')
-  imgsdir = '/IUS/vmr105/rohytg/projects/003_SelfieSeg/009_BgMatch/dataset/PeopleAtLandmarks/corpus'
+  imgsdir = '/IUS/homes4/rohytg/work/data/002_ExtendedPAL/corpus_mods/esvm/test/'
 end
 if ~exist('outdir', 'var')
-  outdir = '/IUS/homes4/rohytg/work/003_DiscoverPatches/results/PeopleAtLandmarks_ESVMModels'
+  outdir = '/IUS/homes4/rohytg/work/data/002_ExtendedPAL/features/ESVM/test/'
 end
 all_imgs = getAllFiles(imgsdir);
 
@@ -29,7 +29,7 @@ for img = all_imgs(:)'
   mkdir(lockoutfpath);
 
   I = imread(fullfile(imgsdir, img{:}));
-  M = im2bw(imread(fullfile(masksdir, img{:})));
+  %M = im2bw(imread(fullfile(masksdir, img{:})));
   disp(['Read ' img{:} '. Computing model...']);
   model = esvm_train_single_exemplar(I, [1 1 size(I, 2) size(I, 1)], negdir);
 %      'mask_img', M); % not masking for now
--- learn/with_hnm/test.m
+++ learn/with_hnm/test.m
@@ -1,17 +1,29 @@
-FEAT = 'fc7';
-featdir = ['~/work/data/001_PALAnd1KHayesDistractor/features/CNN_' FEAT '_text'];
-outdir = ['~/work/data/001_PALAnd1KHayesDistractor/query_scores/' FEAT];
-imgslistfile = '~/work/data/001_PALAnd1KHayesDistractor/ImgsList.txt';
+function test()
+if 0
+  FEAT = 'pool5';
+  featdir = ['~/work/data/001_PALAnd1KHayesDistractor/features/CNN_' FEAT '_text'];
+  outdir = ['~/work/data/001_PALAnd1KHayesDistractor/query_scores/' FEAT];
+  imgslistfile = '~/work/data/001_PALAnd1KHayesDistractor/ImgsList.txt';
+else
+  FEAT = 'fc7_PeopleOnly';
+  featdir = ['~/work/data/002_ExtendedPAL/features/CNN/CNN_' FEAT];
+  outdir = ['~/work/data/002_ExtendedPAL/query_scores/' FEAT];
+  imgslistfile = '~/work/data/002_ExtendedPAL/lists/Images.txt';
+  modelpath = ['/IUS/homes4/rohytg/work/data/002_ExtendedPAL/models/model_' FEAT '.mat'];
+  testimgidspath = '/IUS/homes4/rohytg/work/data/002_ExtendedPAL/lists/NdxesPeopleTest.txt';
+end
+
+testimgids = readList2(testimgidspath);
 
 addpath('bin/');
-load(['model_' FEAT '.mat'], 'llm');
+load(modelpath, 'llm');
 % read images list
 f = fopen(imgslistfile, 'r');
 imgslist = textscan(f, '%s\n');
 fclose(f);
 imgslist = imgslist{1};
 
-for i = 121 : 237
+for i = testimgids(:)'
   im = imgslist{i};
   feats = dlmread(fullfile(featdir, strrep(im, '.jpg', '.txt')));
   feats = bsxfun(@minus, feats, llm.mu);
@@ -22,4 +34,9 @@ for i = 121 : 237
   fprintf('Done for %d\n', i);
 end
 
+function lst = readList2(fpath)
+f = fopen(fpath);
+lst = textscan(f, '%d');
+lst = lst{1};
+fclose(f);
 
